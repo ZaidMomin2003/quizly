a38fddf98498862b25418f9b2209ff989b4d5b7b
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-08-02 06:50:21 +0000

    remove the search bar. i dont want it

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -8,23 +8,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Bell, Search } from 'lucide-react';
+import { Bell } from 'lucide-react';
 import { SidebarTrigger } from '../ui/sidebar';
 import { ThemeToggle } from './ThemeToggle';
-import { Input } from '../ui/input';
 
 export function Header() {
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
        <SidebarTrigger className="md:hidden" />
-      <div className="relative flex-1 md:grow-0">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-        <Input
-          type="search"
-          placeholder="Search..."
-          className="w-full rounded-lg bg-secondary pl-8 md:w-[200px] lg:w-[320px]"
-        />
-      </div>
       <div className="flex items-center gap-4 ml-auto">
         <ThemeToggle />
         <Button variant="ghost" size="icon" className="rounded-full">
