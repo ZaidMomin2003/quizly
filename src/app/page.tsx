66cd62637c015570ab887050ccf567cd8eb8bf8b
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-08-02 18:50:20 +0000

    bypass me authentication and take me to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,22 +20,22 @@ export default function LandingPage() {
         </Link>
         <div className="hidden lg:flex items-center gap-4 ml-auto">
             <Button variant="ghost" asChild>
-              <Link href="/login">
+              <Link href="/dashboard">
                 Login
               </Link>
             </Button>
             <Button asChild>
-              <Link href="/signup">Get Started</Link>
+              <Link href="/dashboard">Get Started</Link>
             </Button>
         </div>
          <nav className="ml-auto flex lg:hidden gap-4 sm:gap-6 items-center">
            <Button variant="ghost" asChild>
-            <Link href="/login">
+            <Link href="/dashboard">
               Login
             </Link>
           </Button>
           <Button asChild>
-            <Link href="/signup">Get Started</Link>
+            <Link href="/dashboard">Get Started</Link>
           </Button>
         </nav>
       </header>
